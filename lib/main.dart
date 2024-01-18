05024f6be2d18e697d35a6a9b61dbaa961f3c03c
Edwin Cardenas <eacardenase@unal.edu.co>
2024-01-17 20:06:12 -0500

    feat: update packages and remove material theme default

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-          useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 147, 229, 250),
             brightness: Brightness.dark,
